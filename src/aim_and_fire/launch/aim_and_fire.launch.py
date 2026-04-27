9699c409e8c23a0f646030a0e7847644dec2510e
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-04-27 15:04:59 +0200

    Update aim_and_fire.launch.py

--- src/aim_and_fire/launch/aim_and_fire.launch.py
+++ src/aim_and_fire/launch/aim_and_fire.launch.py
@@ -1,1 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='aim_and_fire',
+            executable='aim_and_fire',
+            name='aim_and_fire',
+            output='screen',
+            parameters=[
+                {'tolerance_deg': 0.3},
+                {'offset_z': 0.225}
+            ]
+        )
+    ])
